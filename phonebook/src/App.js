94dd88de740d4f45a5b5a0c8e53f984266cce4e4
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-23 01:40:12 +0900

    Create 2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addNumber = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
