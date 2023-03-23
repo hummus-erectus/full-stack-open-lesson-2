0057d6ff73f38b9158e730440fef96a288898eee
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-23 11:53:21 +0900

    Complete 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,23 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '050-1234-4321'
-   }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const addNumber = (e) => {
     e.preventDefault()
 
     if (persons.some(person => person.name === newName)){
       alert(`${newName} already exists in phonebook`)
-    } else{
+    } else {
       const personObject = {
         name: newName,
         number: newNumber,
+        id: persons.length + 1,
       }
       setPersons(persons.concat(personObject))
       setNewName('')
@@ -29,6 +31,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
+      </div>
       <form onSubmit={addNumber}>
         <div>
           name: <input onChange={(e) => setNewName(e.target.value)} value={newName} />
@@ -41,9 +46,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.number}</p>
-      ))}
+      {persons.map((person) => {
+        if (person.name.toLowerCase().includes(searchTerm.toLowerCase())){
+          return <p key={person.name}>{person.name} {person.number}</p>
+        } else {
+          return null
+        }
+      })}
       <div>debug: {newName}</div>
     </div>
   )
